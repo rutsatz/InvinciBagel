7d0c73938c5d7407c2ed91ee9138d3694f4e0999
Rafael Rutsatz <rutsatz@hotmail.com.br>
2018-07-11 22:46:10 -0300

    Adicionado nodes

--- src/invincibagel/InvinciBagel.java
+++ src/invincibagel/InvinciBagel.java
@@ -3,9 +3,13 @@ package invincibagel;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -17,8 +21,48 @@ import javafx.stage.StageStyle;
  */
 public class InvinciBagel extends Application {
 
+    Scene scene;
+    StackPane root;
+    Image splashScreen, instructionLayer, legalLayer, scoresLayer;
+    ImageView splashScreenBackplate, splashScreenTextArea;
+    Button gameButton, helpButton, scoreButton, legalButton;
+    HBox buttonContainer;
+    Insets buttonContainerPadding;
+
     @Override
     public void start(Stage primaryStage) {
+        createSplashScreenNodes();
+        addNodesToStackPane();
+        primaryStage.setTitle("InvinceBagel");
+//        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        gameButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        });
+        helpButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        });
+        scoreButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        });
+        legalButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        });
+
         Button btn = new Button();
         btn.setText("Say 'Hello World'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
@@ -29,17 +73,9 @@ public class InvinciBagel extends Application {
             }
         });
 
-        StackPane root = new StackPane();
-        root.setBackground(Background.EMPTY);
-        root.getChildren().add(btn);
-
-        Scene scene = new Scene(root, 300, 250);
+        scene = new Scene(root, 300, 250);
         scene.setFill(Color.TRANSPARENT);
 
-        primaryStage.setTitle("Hello World!");
-        primaryStage.initStyle(StageStyle.TRANSPARENT);
-        primaryStage.setScene(scene);
-        primaryStage.show();
     }
 
     /**
@@ -47,5 +83,15 @@ public class InvinciBagel extends Application {
      */
     public static void main(String[] args) {
         launch(args);
+    }
+
+    private void createSplashScreenNodes() {
+        root = new StackPane();
+//        root.setBackground(Background.EMPTY);
+//        root.getChildren().add(btn);
+    }
+
+    private void addNodesToStackPane() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
